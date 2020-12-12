353ea392fac71f4cd6f359385897ebb1556acb0c
yukie nameki <y.nameki1205@ymobile.ne.jp>
2020-12-12 12:07:59 +0000

    5.5.4Finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,18 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      # 保存に成功した場合は、ここに記述した処理が実行されます。
       
     else
       render :new
     
     end
   end  
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
