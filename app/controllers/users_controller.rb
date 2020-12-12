c2ecbdabd546491da1721e6686e784c6706900e5
yukie nameki <y.nameki1205@ymobile.ne.jp>
2020-12-12 07:13:21 +0000

    5.5.3Finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # 保存に成功した場合は、ここに記述した処理が実行されます。
+      
+    else
+      render :new
+    
+    end
+  end  
 end
