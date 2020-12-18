7b88d0db6fc946a7059a55a90ebd87f6bcd03390
yukie nameki <y.nameki1205@ymobile.ne.jp>
2020-12-18 11:37:44 +0000

    Finish full-fledged-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,12 @@ class User < ApplicationRecord
   end
   
   def authentiated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end  
+  
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
   
 end
